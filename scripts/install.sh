14ce0ff336b6e37c325d8a7189154702f667f066
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2022-04-02 15:52:16 -0700

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+curl --output ./pastebin.zip --url https://github.com/Quantalabs/pastebin/blob/master/dist/linux.zip?raw=true
+
+echo "Extract pastebin.zip to any directory, and add the executable to path."
