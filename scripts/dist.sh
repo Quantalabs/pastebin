f7c7ce33bfb96c100cb4d8ea299f6b746a68e8d1
QLabs <55121845+Quantalabs@users.noreply.github.com>
2024-07-13 22:15:09 -0700

    build: remove uneeded dependencies

--- scripts/dist.sh
+++
@@ -1,4 +0,0 @@
-pkg index.js -o dist/pastebin --targets node16-linux-x64,node16-macos-x64,node16-win-x64 --compress GZip
-zip -r dist/linux.zip templates dist/pastebin-linux
-zip -r dist/mac.zip templates dist/pastebin-macos
-zip -r dist/win.zip templates dist/pastebin-win.exe
--- scripts/install.sh
+++
@@ -1,3 +0,0 @@
-curl --output ./pastebin.zip --url https://github.com/Quantalabs/pastebin/blob/master/dist/linux.zip?raw=true
-
-echo "Extract pastebin.zip to any directory, and add the executable to path."
